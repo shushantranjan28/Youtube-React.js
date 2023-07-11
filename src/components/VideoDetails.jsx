9a7a133125931bccc6795d726ac4ccb0c0dabcda
shushantranjan28 <shushantranjan.sr@yahoo.com>
2023-07-12 02:33:35 +0530

    yes

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -1,8 +1,12 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { AiOutlineLike } from "react-icons/ai";
+import { abbreviateNumber } from "js-abbreviation-number";
 
 import { fetchDataFromApi } from "../utils/api";
 import { Context } from "../context/contextApi";
+import SuggestionVideoCard from "./SuggestionVideoCard";
 
 const VideoDetails = () => {
     const { id } = useParams();
